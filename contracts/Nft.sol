dbe4e8b5d4179fb12e3de00665c69f54a0498311
KentHirose <k.hirose77777@gmail.com>
2023-08-21 10:38:23 +0900

    Nft mint with URI

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -2,28 +2,112 @@
 pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
+import "./libraries/HexStrings.sol";
 
 contract NFT is ERC721 {
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
-    // publicな関数なので外部からもアクセス可能
-    function mint(address _to, uint256 _tokenId, string memory _tokenURI) external {
-        _mint(_to, _tokenId);
-        _setTokenURI(_tokenId, _tokenURI);
+    uint16 public productCount = 0;
+    using HexStrings for uint256;
+
+    mapping(uint256 => Product) public products;
+
+    struct Product {
+      string name;
+      string image;
+      string description;
+      uint16 price;
+      address seller;
+      address[] stockholders;
+      uint16[] stockholding;
     }
 
-    // _はインターナルな関数を示す
-    function _setTokenURI(uint256 _tokenId, string memory _tokenURI) internal {
-        _tokenURIs[_tokenId] = _tokenURI;
+    address[] stockholders;
+    uint16[] stockholding;
+
+    // stockholdingとstockholdersの初期化
+    function initStockData() public {
+      stockholders = new address[](0);
+      stockholding = new uint16[](0);
     }
 
-    mapping(uint256 => string) private _tokenURIs;
-    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-        return _tokenURIs[_tokenId];
+    // stockholdingとstockholdersの追加
+    function addStockData(address _stockholder, uint16 _stockholding) public {
+      stockholders.push(_stockholder);
+      stockholding.push(_stockholding);
     }
-  
-    // NFTの残高を返す
-    function balance(address _owner) public view returns (uint256) {
-        return balanceOf(_owner);
+
+    function safeMint(
+      address _to, 
+      string memory _name,
+      string memory _image,
+      string memory _description,
+      uint16 _price,
+      address _seller
+    ) public {
+        productCount++;
+        uint256 _tokenId = productCount;
+        _safeMint(_to, _tokenId);
+        products[_tokenId] = Product(
+          _name, 
+          _image, 
+          _description,
+          _price,
+          _seller,
+          stockholders,
+          stockholding
+        );
     }
+
+    function tokenURI(uint256 _tokenId) 
+    public 
+    view 
+    override(ERC721)
+    returns (string memory) {
+        return string(
+          abi.encodePacked(
+          "data:application/json;base64,",
+          Base64.encode(
+            bytes(
+              abi.encodePacked(
+                '{"name":"', products[_tokenId].name, 
+                '", "image":"', products[_tokenId].image,
+                '", "description":"', products[_tokenId].description,
+                '", "price":"', Strings.toString(products[_tokenId].price),
+                '", "seller":"', addressToString(products[_tokenId].seller),
+                '", "stockholders":"', addressArrayToString(products[_tokenId].stockholders),
+                '", "stockholding":"', uintArrayToString(products[_tokenId].stockholding),
+                '"}'
+              )
+            )
+          )
+        ));
+    }
+
+    function addressToString(address addr) internal pure returns (string memory) {
+    return  (uint256(uint160(addr))).toHexString(20);
+    }
+
+    // HexStringを使う
+    function addressArrayToString(address[] memory addrs) internal pure returns (string memory) {
+      string memory str = "";
+      for (uint i = 0; i < addrs.length; i++) {
+        string memory addStr = (uint256(uint160(addrs[i]))).toHexString(20);
+        str = string(abi.encodePacked(str,",", addStr));
+      }
+      return str;
+    }
+
+    function uintArrayToString(uint16[] memory uints) internal pure returns (string memory) {
+      string memory str = "";
+      for (uint i = 0; i < uints.length; i++) {
+        string memory addStr = Strings.toString(uints[i]);
+        str = string(abi.encodePacked(str,",", addStr));
+      }
+      return str;
+    }
+
+
+    
 }
--- test-scripts/nft/nftMint.js
+++ test-scripts/nft/nftMint.js
@@ -5,12 +5,27 @@ module.exports = async function (callback) {
         const nftInstance = await NFT.deployed();
         const accounts = await new web3.eth.getAccounts();
 
-        // 任意のアドレスとトークンID、メタデータのURLを指定
-        const toAddress = accounts[0];
-        const tokenId = 1;
-        const tokenURI = "./images/gakusei_mtg.png"; // 画像データへのURLを含むメタデータのJSONファイル
+        // StockDataの初期化
+        await nftInstance.initStockData();
+        // StockDataの追加
+        // 所持者 accounts[0] 権利割合50 権利者 accounts[1] 権利割合 25 権利者 accounts[2] 権利割合 25
+        await nftInstance.addStockData(accounts[0], 50);
+        await nftInstance.addStockData(accounts[1], 25);
+        await nftInstance.addStockData(accounts[2], 25);
 
-        await nftInstance.mint(toAddress, tokenId, tokenURI);
+        // NFTの発行
+        await nftInstance.safeMint(
+          accounts[0],
+          "学生MTG", 
+          "https://bafybeibsc746kbb4gya4fzqg2zvgfvabfxvgcrn4djx6winjdqqtyzg7ge.ipfs.w3s.link/gakusei_mtg.png", 
+          "学生がMTGをしているイラストのNFT",
+          1000,
+          accounts[0],
+          );
+
+        // NFTのURIを取得
+        const URI = await nftInstance.tokenURI(1);
+        console.log(URI);
         console.log("NFT minted successfully!");
     } catch (error) {
         console.error(error);
